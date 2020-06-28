3359fb53cc595f15d0656f1d838afa4a60b065ac
ramzan07 <ramzan.umt@gmail.com>
2020-06-28 04:10:04 +0200

    empty methods removed

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -110,62 +110,6 @@ class ProxyController extends Controller
         return Redirect::back()->with('success_message', 'The Message');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function handleXRoxy($data, $provider) {
 
         /** Xroxy Handler*/
